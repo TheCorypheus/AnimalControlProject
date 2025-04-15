4ac5c63cd2c0c44a59cba969bb01224689e3ae3d
raheeb <46029484+TheCorypheus@users.noreply.github.com>
2025-04-15 15:11:33 +0200

    level0

--- submission/controller.py
+++ submission/controller.py
@@ -1,7 +1,7 @@
 import numpy as np
 from cobar_miniproject.base_controller import Action, BaseController, Observation
 from .utils import get_cpg, step_cpg
-
+from flygym.examples.locomotion import PreprogrammedSteps
 
 class Controller(BaseController):
     def __init__(
@@ -15,13 +15,30 @@ class Controller(BaseController):
         self.quit = False
         self.cpg_network = get_cpg(timestep=timestep, seed=seed)
         self.preprogrammed_steps = PreprogrammedSteps()
+        self.turn_left_steps = PreprogrammedSteps()
+        self.turn_left_steps.swing_period["RF"] *= 1.5
+        self.turn_left_steps.swing_period["RM"] *= 1.5
+        self.turn_left_steps.swing_period["RH"] *= 1.5
+            
+        self.turn_right_steps = PreprogrammedSteps()
+        self.turn_right_steps.swing_period["LF"] *= 1.5
+        self.turn_right_steps.swing_period["LM"] *= 1.5
+        self.turn_right_steps.swing_period["LH"] *= 1.5
 
     def get_actions(self, obs: Observation) -> Action:
-        joint_angles, adhesion = step_cpg(
+        if(obs["odor_intensity"][0][0] > obs["odor_intensity"][0][1]):
+            
+            joint_angles, adhesion = step_cpg(
             cpg_network=self.cpg_network,
-            preprogrammed_steps=self.preprogrammed_steps,
+            preprogrammed_steps=self.turn_left_steps,
             action=np.array([1.0, 1.0]),
-        )
+            )
+        else:    
+            joint_angles, adhesion = step_cpg(
+                cpg_network=self.cpg_network,
+                preprogrammed_steps=self.turn_right_steps,
+                action=np.array([1.0, 1.0]),
+            )
 
         return {
             "joints": joint_angles,
@@ -33,3 +50,15 @@ class Controller(BaseController):
 
     def reset(self, **kwargs):
         self.cpg_network.reset()
+
+    def turn_left(turn_intensity: float, steps: PreprogrammedSteps) -> PreprogrammedSteps:
+        steps.swing_period["RF"] *= turn_intensity
+        steps.swing_period["RM"] *= turn_intensity
+        steps.swing_period["RH"] *= turn_intensity
+        return steps
+    
+    def turn_right(turn_intensity: float, steps: PreprogrammedSteps) -> PreprogrammedSteps:
+        steps.swing_period["LF"] *= turn_intensity
+        steps.swing_period["LM"] *= turn_intensity
+        steps.swing_period["LH"] *= turn_intensity
+        return steps

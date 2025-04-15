8c40040f1bc511b379def2f62a95b88fef7c20d6
raheeb <46029484+TheCorypheus@users.noreply.github.com>
2025-04-15 18:43:24 +0200

    level1

--- submission/controller.py
+++ submission/controller.py
@@ -2,7 +2,7 @@ import numpy as np
 from cobar_miniproject.base_controller import Action, BaseController, Observation
 from .utils import get_cpg, step_cpg
 from flygym.examples.locomotion import PreprogrammedSteps
-
+np.set_printoptions(threshold=np.inf)
 class Controller(BaseController):
     def __init__(
         self,
@@ -15,7 +15,9 @@ class Controller(BaseController):
         self.quit = False
         self.cpg_network = get_cpg(timestep=timestep, seed=seed)
         self.preprogrammed_steps = PreprogrammedSteps()
-
+        
+        self.coms = np.empty((721, 2))
+        
         turn_intensity = 2.5
 
         self.turn_left_steps = PreprogrammedSteps()
@@ -35,8 +37,20 @@ class Controller(BaseController):
         self.turn_right_steps.swing_period["RH"] /= turn_intensity
 
     def get_actions(self, obs: Observation) -> Action:
-        if(obs["odor_intensity"][0][0] > obs["odor_intensity"][0][1]):
-            
+        visual_information = self.process_visual_observation(obs, 0.06)
+        if(visual_information[2] > 0.01):
+            joint_angles, adhesion = step_cpg(
+                cpg_network=self.cpg_network,
+                preprogrammed_steps=self.turn_right_steps,
+                action=np.array([1.0, 1.0]),
+            )
+        elif(visual_information[5] > 0.01):
+            joint_angles, adhesion = step_cpg(
+            cpg_network=self.cpg_network,
+            preprogrammed_steps=self.turn_left_steps,
+            action=np.array([1.0, 1.0]),
+            )   
+        elif(obs["odor_intensity"][0][0] > obs["odor_intensity"][0][1]):            
             joint_angles, adhesion = step_cpg(
             cpg_network=self.cpg_network,
             preprogrammed_steps=self.turn_left_steps,
@@ -71,3 +85,17 @@ class Controller(BaseController):
         steps.swing_period["LM"] *= turn_intensity
         steps.swing_period["LH"] *= turn_intensity
         return steps
+    
+    #Function adapted from Neuromechfly tutorial
+    def process_visual_observation(self, raw_obs, obj_threshold=0.15):
+        features = np.zeros((2, 3))
+        for i, ommatidia_readings in enumerate(raw_obs["vision"]):
+            is_obj = ommatidia_readings.max(axis=1) < obj_threshold
+            is_obj_coords = self.coms[is_obj]
+            if is_obj_coords.shape[0] > 0:
+                features[i, :2] = is_obj_coords.mean(axis=0)
+            features[i, 2] = is_obj_coords.shape[0]    
+        features[:, 0] /= 450 # normalize y_center
+        features[:, 1] /= 512  # normalize x_center
+        features[:, 2] /= 721  # normalize area
+        return features.ravel()
